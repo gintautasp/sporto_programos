3cefb1f834c482714fef6a055f3337105453da1c
Mavilius <lucid_spark@yahoo.com>
2021-05-13 09:02:33 +0300

    linux phpmyadmin user

--- tmp_failai/db_changes.sql
+++ tmp_failai/db_changes.sql
@@ -6,4 +6,5 @@ ALTER TABLE `raumenys` ADD FOREIGN KEY (`id_raumenu_grupes`) REFERENCES `raumenu
 
 ALTER TABLE `treniruociu_planai` ADD `rusis` ENUM('cardio','jegos','bfp','ištvermes','mases auginimas','raumenu ryškinimas','atstatomoji','lankstumo','svorio metimo','kita') NULL DEFAULT 'kita' AFTER `pav`, ADD INDEX (`rusis`);
 
-CREATE USER 'sportas'@'%' IDENTIFIED VIA mysql_native_password USING '***';GRANT ALL PRIVILEGES ON *.* TO 'sportas'@'%' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+CREATE USER 'sportas'@'%' IDENTIFIED BY '***';
+GRANT ALL PRIVILEGES ON *.* TO 'sportas'@'%';
